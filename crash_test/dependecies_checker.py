a1441b9e7f453f371edf14c2f7a7dbad8899e09d
utox39 <francescomoccaldi39@gmail.com>
2023-12-01 17:56:59 +0100

    Add error logger implementation

--- crash_test/dependecies_checker.py
+++ crash_test/dependecies_checker.py
@@ -1,12 +1,23 @@
 #!/usr/bin/env python3
 
 import os
+from typing import Final
 
-from .script_generator import script_generator
+from crash_test.error_logger import log_error
+from crash_test.script_generator import script_generator
+
+YELLOW: Final[str] = "\033[1m\033[33m"
+NC: Final[str] = "\033[0m"
 
 
 def check_dependencies(project_path: str) -> str:
-    project_name = project_path[project_path.rfind("/") + 1:]
-    for element in os.listdir(project_path):
-        if element == "requirements.txt":
-            return script_generator(project_name=project_name, project_type="python")
+    if os.path.exists(project_path):
+        project_name = project_path[project_path.rfind("/") + 1:]
+        for element in os.listdir(project_path):
+            match element:
+                case "requirements.txt":
+                    return script_generator(project_name=project_name, project_type="python")
+                case _:
+                    print(log_error(error_code=306))
+    else:
+        print(log_error(error_code=300, project_path=project_path))
