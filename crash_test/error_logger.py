8574ef5746aa4d5f72c809d68ee1f5ea7a4ec6a4
utox39 <francescomoccaldi39@gmail.com>
2023-12-02 16:34:23 +0100

    Add colorama

--- crash_test/error_logger.py
+++ crash_test/error_logger.py
@@ -1,13 +1,8 @@
 #!/usr/bin/env python3
 
-from typing import Final
+from colorama import Fore, Style
 
 import crash_test.error_codes
-
-YELLOW: Final[str] = "\033[1m\033[33m"
-RED: Final[str] = "\033[0;31m"
-# No colors
-NC: Final[str] = "\033[0m"
 
 
 def log_error(error_code: int, project_path: str = "") -> str:
@@ -17,12 +12,12 @@ def log_error(error_code: int, project_path: str = "") -> str:
         case crash_test.error_codes.SINGLE_FILE_ERROR:
             return "Please only provide project directories. Single files are not allowed."
         case crash_test.error_codes.MULTIPASS_GENERIC_ERROR:
-            return f"{RED}multipass: error: "
+            return f"{Fore.RED}multipass: error: "
         case crash_test.error_codes.MULTIPASS_NOT_INSTALLED_ERROR:
             return "crashtest: error: Multipass is not installed!"
         case crash_test.error_codes.INVALID_INSTANCE_NAME_ERROR:
             return "crashtest: error: invalid instance name. The instance name should be like this: "
         case crash_test.error_codes.NO_DEPENDENCIES_FOUND_ERROR:
-            return f"{YELLOW}No dependencies detected.{NC}\n"
+            return f"{Fore.YELLOW}No dependencies detected.{Style.RESET_ALL}\n"
         case crash_test.error_codes.NO_SUPPORTED_REQUIREMENTS_FILE_FOUND_ERROR:
-            return f"{YELLOW}No supported project requirements file was found!{NC}\n"
+            return f"{Fore.YELLOW}No supported project requirements file was found!{Style.RESET_ALL}\n"
