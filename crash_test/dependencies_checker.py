d934132c8e487e07d5e88b7a9d51d717bf377201
utox39 <francescomoccaldi39@gmail.com>
2023-12-21 01:58:15 +0100

    Add a function that searches for requirements files

--- crash_test/dependencies_checker.py
+++ crash_test/dependencies_checker.py
@@ -7,14 +7,22 @@ from crash_test.error_logger import log_error
 from crash_test.script_generator import script_generator
 
 
-def check_dependencies(project_path: str) -> str:
+def find_requirements_file(project_path):
     if os.path.exists(project_path):
-        project_name = project_path[project_path.rfind("/") + 1:]
-        for element in os.listdir(project_path):
-            match element:
+        files = [element for element in os.listdir(project_path) if element is not os.path.isdir(element)]
+        for file in files:
+            match file:
                 case "requirements.txt":
-                    return script_generator(project_name=project_name, project_type="python")
-                case _:
-                    print(log_error(error_code=crash_test.error_codes.NO_SUPPORTED_REQUIREMENTS_FILE_FOUND_ERROR))
+                    return "python"
+
+
+def check_dependencies(project_path: str, project_name: str) -> str:
+    if os.path.exists(project_path):
+        project_type = find_requirements_file(project_path=project_path)
+        if project_type:
+            return script_generator(project_name=project_name, project_type=project_type)
+        else:
+            print(log_error(error_code=crash_test.error_codes.NO_SUPPORTED_REQUIREMENTS_FILE_FOUND_ERROR))
+
     else:
         print(log_error(error_code=crash_test.error_codes.NO_SUCH_FILE_OR_DIRECTORY_ERROR, project_path=project_path))
