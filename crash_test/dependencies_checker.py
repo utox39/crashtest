e24e9fd56edd3eccf4c5ee09decc2522c4858fcb
utox39 <francescomoccaldi39@gmail.com>
2023-12-23 15:27:18 +0100

    Add implementation of script_selector and utils

--- crash_test/dependencies_checker.py
+++ crash_test/dependencies_checker.py
@@ -4,7 +4,11 @@ import os
 
 import crash_test.error_codes
 from crash_test.error_logger import log_error
-from crash_test.script_generator import script_generator
+from crash_test.script_selector import script_selector
+from crash_test.utils import get_scripts_absolute_path, check_scripts_path
+
+
+# TODO: 2 Add support for npm
 
 
 def find_requirements_file(project_path):
@@ -16,13 +20,15 @@ def find_requirements_file(project_path):
                     return "python"
 
 
-def check_dependencies(project_path: str, project_name: str) -> str:
+def check_dependencies(project_path: str, scripts_relative_path) -> str:
     if os.path.exists(project_path):
         project_type = find_requirements_file(project_path=project_path)
+        scripts_absolute_path = get_scripts_absolute_path(relative_path=scripts_relative_path)
         if project_type:
-            return script_generator(project_name=project_name, project_type=project_type)
+            if check_scripts_path(script_path=scripts_absolute_path):
+                return script_selector(project_type=project_type,
+                                       scripts_path=scripts_absolute_path)
         else:
             print(log_error(error_code=crash_test.error_codes.NO_SUPPORTED_REQUIREMENTS_FILE_FOUND_ERROR))
-
     else:
         print(log_error(error_code=crash_test.error_codes.NO_SUCH_FILE_OR_DIRECTORY_ERROR, project_path=project_path))
